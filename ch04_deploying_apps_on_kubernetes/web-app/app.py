2fe55ffe5811773eef950a3d41d19cc6ac1db6dd
nanjekyejoannah <nanjekyejoannah@gmail.com>
2018-12-13 17:20:05 +0300

    chapter 4

---
+++ ch04_deploying_apps_on_kubernetes/web-app/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from redis import Redis
+import os
+app = Flask(__name__)
+redis = Redis(host=os.environ.get('REDIS_HOST', 'redis'), port=6379)
+
+@app.route('/')
+def index():
+    return 'Hey you'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
